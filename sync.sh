218050c861d4eb3408eb16368063a650015c6de4
jmcb <joelsgp@protonmail.com>
2023-04-11 20:52:54 +0100

    Remove that bit

--- sync.sh
+++ sync.sh
@@ -10,7 +10,4 @@ do
     pkgbuildir="$dirbase/pkgbuild-$temp"
     cp -ut "$pkgbuildir" ${common[*]}
     cp -u "PKGBUILD.$temp" "$pkgbuildir/PKGBUILD"
-    git="git -C $pkgbuildir"
-    $git add .
-    $git diff-index --quiet HEAD || git commit -m 'Update template'
 done
