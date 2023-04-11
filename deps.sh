29159ccc1a0547714dc6e5fae981df6f03f5301c
jmcb <joelsgp@protonmail.com>
2023-04-11 23:54:14 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-set -ex
-pacman -Syu base-devel pacman-contrib asp namcap desktop-file-utils
+set -eux
+# also install paru for handling dependencies that are in the aur
+pacman -Syu base-devel pacman-contrib devtools asp pkgfile namcap gendesk desktop-file-utils
 # add this to your shell startup scripts!
-alias printsrcinfo="updpkgsums && makepkg --printsrcinfo > .SRCINFO"
+# alias printsrcinfo="updpkgsums && makepkg --printsrcinfo > .SRCINFO"
