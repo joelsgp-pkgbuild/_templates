3829bb045098cb6916ab9b4c32645d5ba7067de0
jmcb <joelsgp@protonmail.com>
2023-04-29 20:24:28 +0100

    Working on new script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 set -eux
-# also install paru for handling dependencies that are in the aur
+# you'll also want to install paru for handling dependencies that are in the aur
 pacman -Syu base-devel pacman-contrib devtools asp pkgfile namcap gendesk desktop-file-utils
-# add this to your shell startup scripts!
+# then add this to your shell startup scripts!
 # alias printsrcinfo="updpkgsums && makepkg --printsrcinfo > .SRCINFO"
